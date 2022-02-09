4593cff31b86b8bd493e8bbf985d4daf2ecf8b3f
costagil74 <eusoude74@gmail.com>
2022-02-09 13:42:46 -0300

    condicoes-If-e-Else

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const provaTecnica = 59;
+
+if (provaTecnica >= 80 ) {
+  console.log ("Parabéns, você foi aprovado (a)!");
+} else if (provaTecnica >= 60) {
+  console.log ("Você está na nossa lista de espera");
+} else {
+  console.log ("Você foi reprovada(o)");
+}
+
